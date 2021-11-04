61bb744490dd30ab7eedda447e2dfc576f674781
Muhammadirvanzaeni04 <92837211+Muhammadirvanzaeni04@users.noreply.github.com>
2021-11-04 13:01:57 +0700

    Add files via upload

---
+++ pengenalan.py
@@ -0,0 +1,11 @@
+print ("menghitung luas lingkaran")
+r = int (input("masukan jari jari lingkaran : "))
+phi = 3.14
+luas = phi * r * r
+print ("luas lingkaran adalah", luas )
+
+print ("menghitung keliling lingkaran")
+r = int (input("masukan jari jari lingkaran : "))
+phi = 3.14
+keliling = 2 * phi * r
+print ("keliling lingkaran adalah", keliling )
